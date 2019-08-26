9557bd669f8c6ccc9d84205af33b2761719200a1
Nessumsar <54313842+Nessumsar@users.noreply.github.com>
2019-08-26 16:05:52 +0200

    Update App.java

--- src/main/java/se/lexicon/LukasRasmussen/App.java
+++ src/main/java/se/lexicon/LukasRasmussen/App.java
@@ -1,85 +1,75 @@
 package se.lexicon.LukasRasmussen;
-
 import java.util.Scanner;
 
 
 public class App 
 {
+   private static Scanner myScan = new Scanner(System.in);
     public static void main( String[] args )
     {
-        boolean nej = true;
-        Scanner myScan = new Scanner(System.in);
-        //while?
-        while(nej){
-            String avslut = "Q";
-            Double res;
+
+        boolean sluta = true;
+        while (sluta) {
 
             System.out.println("Skriv ett tal. Skriv Q för att avsluta.");
             String firstnum = myScan.nextLine();
             if (firstnum.equalsIgnoreCase("Q")){
-                System.exit(0);
+                System.out.println("Avslutar miniräknaren.");
+                break;
             }
 
             System.out.println("Skriv ett tal till. Skriv Q för att avsluta.");
             String secondnum = myScan.nextLine();
             if (secondnum.equalsIgnoreCase("Q")){
-                System.exit(0);
+                System.out.println("Avslutar miniräknaren.");
+                break;
             }
 
             System.out.println("Välj ett räknesätt. Skriv Q för att avsluta.");
             String operations = myScan.nextLine();
             if (operations.equalsIgnoreCase("Q")){
-                System.exit(0);
+                System.out.println("Avslutar miniräknaren.");
+                break;
             }
-
-            //if inkommande line int -> fortsätt, else string -> avslut?
-           // if (firstnum.equalsIgnoreCase(avslut) || secondnum.equalsIgnoreCase(avslut) || operations.equals(avslut)) {
-             //   System.exit(0);
-           // }
-            switch(firstnum){
-                case "Q":
-                case "q":
-                    System.out.println("Avslutar miniräknaren.");
-                    nej = false;
-                    break;
-                default:
-
-            }
-
-            //while loop? ej loop i loop dock
             switch(operations) {
                 case "+":
-                    Double convert1 = Double.parseDouble(firstnum);
-                    Double convert2 = Double.parseDouble(secondnum);
-                    res = convert1 + convert2;
-                    System.out.println(convert1+" plus "+convert2+" är "+res);
+                    System.out.println("Resultatet är "+resplus(stringToDouble(firstnum), stringToDouble(secondnum)));
+                    System.out.println(" ");
                     break;
                 case "-":
-                    convert1 = Double.parseDouble(firstnum);
-                    convert2 = Double.parseDouble(secondnum);
-                    res = convert1 - convert2;
-                    System.out.println(convert1+" minus "+convert2+" är "+res);
+                    System.out.println("Resultatet är "+resmin(stringToDouble(firstnum), stringToDouble(secondnum)));
+                    System.out.println(" ");
                     break;
                 case "*":
-                    convert1 = Double.parseDouble(firstnum);
-                    convert2 = Double.parseDouble(secondnum);
-                    res = convert1 * convert2;
-                    System.out.println(convert1+" gånger "+convert2+" är "+res);
+                    System.out.println("Resultatet är "+resggr(stringToDouble(firstnum), stringToDouble(secondnum)));
+                    System.out.println(" ");
                     break;
                 case "/":
-                    convert1 = Double.parseDouble(firstnum);
-                    convert2 = Double.parseDouble(secondnum);
-                    res = convert1 / convert2;
-                    System.out.println(convert1+" delat med  "+convert2+" är "+res);
+                    System.out.println("Resultatet är "+resdiv(stringToDouble(firstnum), stringToDouble(secondnum)));
+                    System.out.println(" ");
                     break;
-                case "Q":
-                        System.out.println("");
                 default:
                     System.out.println(operations + " är inte ett giltigt räknesätt.");
                     System.out.println(" ");
             }
         }
+    }
 
+    public static double stringToDouble(String num){
+        Double convert = Double.parseDouble(num);
+        return convert;
+    }
+    public static double resplus(double num1, double num2){
+        return num1+num2;
+    }
+    public static double resmin(double num1, double num2){
+        return num1-num2;
+    }
+    public static double resggr(double num1, double num2){
+        return num1*num2;
+    }
+    public static double resdiv(double num1, double num2){
+        return num1/num2;
+    }
 
     }
-}
